d741b701b7547688af87eb8f083a25e6e55b6841
zsoldosp <peter.zsoldos@gmail.com>
2014-04-16 20:45:56 +0200

    extracted non-app behavior from taskboard.tests.lib
    funny how it exposes how the test tools are often not
    tested in their own right, but simply relied on the tests
    working to implicitly test their behavior

--- polytesting/__init__.py
+++ polytesting/__init__.py
@@ -5,6 +5,12 @@ import tempfile
 import traceback
 from bs4 import BeautifulSoup
 from enhance_exception import enhance_exception
+from django.utils.decorators import classonlymethod
+from django.conf import settings
+from django.test import LiveServerTestCase
+from django.core.urlresolvers import clear_url_caches, set_urlconf
+from django.conf.urls import patterns
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class SoupSelectionList(list):
@@ -94,3 +100,105 @@ class JsonLoader(object):
     def json_loads(self, content):
         with enhance_exception(lambda: content):
             return json.loads(content, object_pairs_hook=od)
+
+class AllowRichEqualityAssertions(object):
+    def _getAssertEqualityFunc(self, first, second):
+        """ see  http://bugs.python.org/issue2578 - by design it
+            only uses rich assertion if the two objects are the
+            exact same type """
+        if isinstance(second, type(first)): # I use the expected, actual convention
+            asserter = self._type_equality_funcs.get(type(first))
+            if asserter is not None and isinstance(asserter, basestring):
+                    return getattr(self, asserter)
+        return super(AllowRichEqualityAssertions, self)._getAssertEqualityFunc(first, second)
+
+
+class PolyTestCase(AllowRichEqualityAssertions):
+    @classonlymethod
+    def setUpClass(cls):
+        super(PolyTestCase, cls).setUpClass()
+        cls.start_liveserver_if_needed()
+        cls.setup_root_urlconf_if_needed()
+
+    @classonlymethod
+    def setup_root_urlconf_if_needed(cls):
+        urlpatterns = list(
+            test_cls.urls.urlpatterns 
+            for test_cls in cls.get_test_api_classes()
+            if hasattr(test_cls, 'urls'))
+        consolidated = reduce(lambda a, b: a + b, urlpatterns, [])
+        class urls:
+            urlpatterns = consolidated
+        cls.urls = urls
+
+        def change_root_urlconf_to(urls):
+            # TODO: copied from django.test.SimpleTestCase._urlconf_setup - PR upstairs to make it availabel outside testing too?
+            set_urlconf(None)
+            settings.ROOT_URLCONF = urls
+            clear_url_caches() 
+
+        change_root_urlconf_to(cls.urls)
+
+    @classonlymethod
+    def tearDownClass(cls):
+        super(PolyTestCase, cls).tearDownClass()
+        cls.stop_liveserver_if_needed()
+
+    @classonlymethod
+    def get_test_api_class_names(cls):
+        return filter(lambda name: name.endswith('_cls'), dir(cls))
+
+    def setUp(self):
+        super(PolyTestCase, self).setUp()
+        cls = type(self)
+        cls.reset_webdriver_if_needed()
+        polytest_created_objects = []
+        for prop_cls_name in cls.get_test_api_class_names():
+            prop_name = prop_cls_name[:-4]
+            try:
+                tp_to_create = getattr(cls, prop_cls_name)
+                obj = tp_to_create(self)
+                if hasattr(obj, 'given_a_board'):
+                    polytest_created_objects.append(obj)
+            except TypeError as e:
+                raise TypeError(
+                    'could not create instance of %s (from %s.%s) - %s' % (
+                        tp_to_create, cls, prop_cls_name, e
+                    )
+                )
+            setattr(self, prop_name, obj)
+        self.polytest_created_objects = polytest_created_objects 
+
+    @classonlymethod
+    def get_test_api_classes(cls):
+        return map(
+            lambda name: getattr(cls, name),
+            cls.get_test_api_class_names())
+
+    @classonlymethod
+    def start_liveserver_if_needed(cls):
+        cls.liveserver_url = None
+        for api_cls in cls.get_test_api_classes():
+            if not getattr(api_cls, 'needs_live_server', False):
+                continue
+            if cls.liveserver_url is None:
+                LiveServerTestCase.setUpClass()
+                cls.selenium = WebDriver()
+                # inlined from LiveServerTestCase
+                cls.liveserver_url = 'http://%s:%s' % (
+                    LiveServerTestCase.server_thread.host, LiveServerTestCase.server_thread.port)
+            api_cls.server_thread = LiveServerTestCase.server_thread
+            api_cls.selenium = cls.selenium
+            api_cls.liveserver_url = cls.liveserver_url
+        cls.has_liveserver = cls.liveserver_url is not None
+        
+    @classonlymethod
+    def stop_liveserver_if_needed(cls):
+        if cls.has_liveserver:
+            LiveServerTestCase.tearDownClass()
+            cls.selenium.quit()
+
+    @classonlymethod
+    def reset_webdriver_if_needed(cls):
+        if cls.has_liveserver:
+            cls.selenium.get('about:blank')
--- taskboard/tests/lib.py
+++ taskboard/tests/lib.py
@@ -1,118 +1,27 @@
 from django.utils.decorators import classonlymethod
-from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
 import taskboard
-from django.core.urlresolvers import clear_url_caches, set_urlconf
-from django.conf.urls import patterns
-from django.conf import settings
+from polytesting import PolyTestCase
 
 
-class BaseBoardTestCaseMixin(object):
-
-    def _getAssertEqualityFunc(self, first, second):
-        """ see  http://bugs.python.org/issue2578 - by design it
-            only uses rich assertion if the two objects are the
-            exact same type """
-        if isinstance(second, type(first)): # I use the expected, actual convention
-            asserter = self._type_equality_funcs.get(type(first))
-            if asserter is not None and isinstance(asserter, basestring):
-                    return getattr(self, asserter)
-        return super(BaseBoardTestCaseMixin, self)._getAssertEqualityFunc(first, second)
+class BoardApi(PolyTestCase):
 
     @classonlymethod
     def setUpClass(cls):
-        super(BaseBoardTestCaseMixin, cls).setUpClass()
+        super(BoardApi, cls).setUpClass()
         cls.orig_board_loader = taskboard.board_loader
-        cls.start_liveserver_if_needed()
-        cls.setup_root_urlconf_if_needed()
-
-    @classonlymethod
-    def setup_root_urlconf_if_needed(cls):
-        urlpatterns = list(
-            test_cls.urls.urlpatterns 
-            for test_cls in cls.get_test_api_classes()
-            if hasattr(test_cls, 'urls'))
-        consolidated = reduce(lambda a, b: a + b, urlpatterns, [])
-        class urls:
-            urlpatterns = consolidated
-        cls.urls = urls
-
-        def change_root_urlconf_to(urls):
-            # TODO: copied from django.test.SimpleTestCase._urlconf_setup - PR upstairs to make it availabel outside testing too?
-            set_urlconf(None)
-            settings.ROOT_URLCONF = urls
-            clear_url_caches() 
-
-        change_root_urlconf_to(cls.urls)
 
     @classonlymethod
     def tearDownClass(cls):
-        super(BaseBoardTestCaseMixin, cls).tearDownClass()
+        super(BoardApi, cls).tearDownClass()
         taskboard.board_loader = cls.orig_board_loader
-        cls.stop_liveserver_if_needed()
 
     def setUp(self):
-        super(BaseBoardTestCaseMixin, self).setUp()
-        cls = type(self)
-        cls.reset_webdriver_if_needed()
-        board_builder_objs = []
-        for prop_cls_name in cls.get_test_api_class_names():
-            prop_name = prop_cls_name[:-4]
-            try:
-                tp_to_create = getattr(cls, prop_cls_name)
-                obj = tp_to_create(self)
-                if hasattr(obj, 'given_a_board'):
-                    board_builder_objs.append(obj)
-            except TypeError as e:
-                raise TypeError(
-                    'could not create instance of %s (from %s.%s) - %s' % (
-                        tp_to_create, cls, prop_cls_name, e
-                    )
-                )
-            setattr(self, prop_name, obj)
+        super(BoardApi, self).setUp()
+        board_builder_objs = list(o for o in self.polytest_created_objects if hasattr(o, 'given_a_board'))
         self.assertEquals(1, len(board_builder_objs), 'only one helper should build the board, but got multiple: %s' % ', '.join('%s' % type(x) for x in board_builder_objs))
         taskboard.board_loader = board_builder_objs[0]
 
-    @classonlymethod
-    def get_test_api_class_names(cls):
-        return filter(lambda name: name.endswith('_cls'), dir(cls))
 
-    @classonlymethod
-    def get_test_api_classes(cls):
-        return map(
-            lambda name: getattr(cls, name),
-            cls.get_test_api_class_names())
-
-    @classonlymethod
-    def start_liveserver_if_needed(cls):
-        cls.liveserver_url = None
-        for api_cls in cls.get_test_api_classes():
-            if not getattr(api_cls, 'needs_live_server', False):
-                continue
-            if cls.liveserver_url is None:
-                LiveServerTestCase.setUpClass()
-                cls.selenium = WebDriver()
-                # inlined from LiveServerTestCase
-                cls.liveserver_url = 'http://%s:%s' % (
-                    LiveServerTestCase.server_thread.host, LiveServerTestCase.server_thread.port)
-            api_cls.server_thread = LiveServerTestCase.server_thread
-            api_cls.selenium = cls.selenium
-            api_cls.liveserver_url = cls.liveserver_url
-        cls.has_liveserver = cls.liveserver_url is not None
-        
-    @classonlymethod
-    def stop_liveserver_if_needed(cls):
-        if cls.has_liveserver:
-            LiveServerTestCase.tearDownClass()
-            cls.selenium.quit()
-
-    @classonlymethod
-    def reset_webdriver_if_needed(cls):
-        if cls.has_liveserver:
-            cls.selenium.get('about:blank')
-
-
-class BoardApi(BaseBoardTestCaseMixin):
     def get_tasks_for(self, owner, status):
         return self.getter.get_tasks_for(owner, status)
 
