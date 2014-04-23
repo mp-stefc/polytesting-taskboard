6116321694bd5220df3c6435595f28dfae591d7a
Peter Zsoldos <peter.zsoldos@gmail.com>
2014-04-23 16:42:05 +0200

    it only works with python 2.7

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+python --version 2>&1 | grep 2.7 
+exit_code=$?
+if [ "$exit_code" != "0" ]]; then
+    echo "This only works with Python 2.7"
+    exit $exit_code
+fi
 (python -m unittest discover -s enhance_exception $*) && \
 (python -m unittest discover -s polytesting $*) && \
     ./manage.py test taskboard.tests $*
