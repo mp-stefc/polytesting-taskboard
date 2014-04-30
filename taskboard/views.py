c2cbb4d69cd1d53c9862dc89944ae2aaf749d11f
zsoldosp <peter.zsoldos@gmail.com>
2014-04-30 20:23:43 +0200

    removed now unneeded app behavior after the tests are gone

--- taskboard/views.py
+++ taskboard/views.py
@@ -3,27 +3,17 @@ from django.views.generic import FormView
 from django import forms
 from django.core.urlresolvers import reverse
 import taskboard
-from taskboard.response import JsonResponse
 
 
 class TaskBoardView(TemplateView):
 
     template_name = 'taskboard/board.html'
-    response_format = None
     
-    def render_to_response(self, context, **response_kwargs):
-        if self.response_format == 'json':
-            board = context['board']
-            return JsonResponse(obj_to_dump=board.as_nested_dict(), **response_kwargs)
-        return super(TaskBoardView, self).render_to_response(context, **response_kwargs)
-
     def get_context_data(self, **kwargs):
         return super(TaskBoardView, self).get_context_data(
             board=taskboard.board_loader.get_board(),
             **kwargs
         )
-
-    # TODO: django.serializers.json, and the details fo TemplateView.reder_to_response - class.content_type and class.content_renderer
 
 
 class MoveTaskView(FormView):
